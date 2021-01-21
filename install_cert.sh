209fa5759afd7afb59f083e1644b85340d6f4462
gactocat <gactocat@gmail.com>
2021-01-22 00:55:11 +0900

    🔧  Add support for CI env.

--- install_cert.sh
+++ install_cert.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+if ! command -v mkcert > /dev/null 2>&1; then
+    echo "\`mkcert\` not found. Nothing to do."
+    exit 0
+fi
+
 mkcert -install \
     -cert-file cert.pem \
     -key-file key.pem \
